71e98b183ba75c8933eb81df6a10b7ab6d6abee2
Logan <logan@Logan.Linux>
2019-03-10 17:25:16 -0600

    Main window is now creating walls and displaying them

---
+++ ColoringBook.py
@@ -0,0 +1,17 @@
+from tkinter import Frame
+from Wall import Wall
+from Route import Route
+
+class ColoringBook(Frame):
+    def __init__(self,master):
+        Frame.__init__(self)
+        self.master = master
+        master.title("Coloring Book Plus")
+
+        #initialize walls
+        self.leftwall = Wall("walls/left_wall_bolts.gif",self)
+        self.rightwall = Wall("walls/right_wall_bolts.gif",self)
+        self.leftwall.pack(side="left")
+
+        #start a new route
+        newroute = Route()
