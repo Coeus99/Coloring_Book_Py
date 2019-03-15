1d981be6db1b5b83c106e9d9363c154baf4549a5
Logan <logan@Logan.Linux>
2019-03-15 13:05:32 -0600

    can now open a temporary saved file. Next up we need file names!

--- Hold.py
+++ Hold.py
@@ -6,19 +6,3 @@ class Hold:
         self.wall = "left"
         self.position = [0,0,0]
         self.RGBA = [0,0,0,255]
-    
-    def get_imagetk(self,scale=1.0):
-        img = Image.open(self.modelpath)
-        scaledsize = int(img.width*scale),int(img.height*scale)
-        scaledimg = img.resize(scaledsize)
-        rotatedimg = (scaledimg.convert("RGBA")).rotate(self.position[2],expand=True)
-        recoloredimgdata = []
-        recoloredimg = Image.new("RGBA",rotatedimg.size)
-        for pixel in rotatedimg.getdata():
-            if pixel == (255,255,255,255):
-                recoloredimgdata.append(tuple(self.RGBA))
-            else:
-                recoloredimgdata.append(pixel)
-        recoloredimg.putdata(recoloredimgdata)
-        self.imgtk = ImageTk.PhotoImage(recoloredimg)
-        return self.imgtk
