0db6d9f454a0bb3733034c94258b66968007bf28
Logan <logan@Logan.Linux>
2019-03-23 09:55:38 -0600

    Removed xcf files for jugs, added better shading for jugs and added jibs

--- utilities.py
+++ utilities.py
@@ -13,12 +13,17 @@ def get_hold_imagetk(hold,scale=1.0):
     recoloredimgdata = []
     for pixel in rotatedimg.getdata():
         pixel = list(pixel)
-        brightness = (pixel[0] + pixel[1] + pixel[2])/765
         #brightness = 0 : black : 0,0,0
         #brightness = 1 : color : r,g,b
-        pixel[0] = int(hold.RGB[0] * brightness)
-        pixel[1] = int(hold.RGB[1] * brightness)
-        pixel[2] = int(hold.RGB[2] * brightness)
+        pixel[0] = int(hold.RGB[0] - (255 - pixel[0]))
+        if (pixel[0] > 255):
+            pixel[0] = 255
+        pixel[1] = int(hold.RGB[1] - (255 - pixel[1]))
+        if (pixel[1] > 255):
+            pixel[1] = 255
+        pixel[2] = int(hold.RGB[2] - (255 - pixel[2]))
+        if (pixel[2] > 255):
+            pixel[2] = 255
         recoloredimgdata.append(tuple(pixel))
 
     recoloredimg.putdata(recoloredimgdata)
