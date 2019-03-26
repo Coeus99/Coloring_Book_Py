4dd852a6de1fcf1b467ed9264cacfc60682b4931
Coeus99 <logan.grosz@mines.sdsmt.edu>
2019-03-26 13:01:53 -0600

    Added grade and grade changing. Bug: cancel doesn't actually cancel. Whoops, this should be like editholdpopup instead

--- EditRoutePopup.py
+++ EditRoutePopup.py
@@ -1,4 +1,4 @@
-from tkinter import Toplevel,Entry,Button,Listbox
+from tkinter import Toplevel,Entry,Button,Listbox,Message
 
 class EditRoutePopup(Toplevel):
     def __init__(self,master):
@@ -16,6 +16,12 @@ class EditRoutePopup(Toplevel):
         self.name_entry.grid(row=0,column=1)
 
         #grade with - on left + on right
+        self.grade_message = Message(self,text=self.currentroute.grade[0]+str(self.currentroute.grade[1])+self.currentroute.grade[2])
+        self.grade_message.grid(row=1,column=1)
+        self.gradeplus_buttonbutton = Button(self,text="+",command=self.on_gradeplus)
+        self.gradeplus_buttonbutton.grid(row=1,column=2)
+        self.grademinus_buttonbutton = Button(self,text="-",command=self.on_grademinus)
+        self.grademinus_buttonbutton.grid(row=1,column=0)
 
         #holds
 
@@ -24,22 +30,22 @@ class EditRoutePopup(Toplevel):
         self.taglist.insert('end', "Tags:")
         for tag in self.currentroute.tags:
             self.taglist.insert('end',tag)
-        self.taglist.grid(row=1,column=0,columnspan=3)
+        self.taglist.grid(row=2,column=0,columnspan=3)
 
         #add a tag
         self.tag_entry = Entry(self)
         self.tag_entry.insert(0,"Enter tag here.")
-        self.tag_entry.grid(row=2,column=0)
+        self.tag_entry.grid(row=3,column=0)
         self.add_tag_button = Button(self,text="Add Tag",command=self.add_tag)
-        self.add_tag_button.grid(row=2,column=1)
+        self.add_tag_button.grid(row=3,column=1)
         self.rem_tag_button = Button(self,text="Remove Tag",command=self.rem_tag)
-        self.rem_tag_button.grid(row=2,column=2)
+        self.rem_tag_button.grid(row=3,column=2)
 
         #cancel and ok
         cancel_button = Button(self, text="Cancel", command = self.on_cancel)
-        cancel_button.grid(row=3,column=2)
+        cancel_button.grid(row=4,column=2)
         ok_button = Button(self, text="Ok", command=self.on_ok)
-        ok_button.grid(row=3,column=3)
+        ok_button.grid(row=4,column=3)
 
     def show(self):
         self.transient(self.master)
@@ -63,6 +69,22 @@ class EditRoutePopup(Toplevel):
         self.taglist.delete(1,'end')
         for tag in self.currentroute.tags:
             self.taglist.insert('end',tag)
+
+    def on_gradeplus(self):
+        numgrade = self.currentroute.grade[1]
+        tempgrade = list(self.currentroute.grade)
+        if (numgrade < 16):
+            tempgrade[1] = numgrade + 1
+            self.currentroute.grade = tuple(tempgrade)
+            self.grade_message.configure(text=tempgrade[0]+str(tempgrade[1])+tempgrade[2])
+            
+    def on_grademinus(self):
+        numgrade = self.currentroute.grade[1]
+        tempgrade = list(self.currentroute.grade)
+        if (numgrade > 0 ):
+            tempgrade[1] = numgrade - 1
+            self.currentroute.grade = tuple(tempgrade)
+            self.grade_message.configure(text=tempgrade[0]+str(tempgrade[1])+tempgrade[2])
 
     def on_cancel(self):
         self.destroy()
